096e571d0223dd97a9151bc48d6c662102a55368
salkhokhar <61519106+salkhokhar@users.noreply.github.com>
2022-11-19 23:40:27 +0530

    Create CompressProjects.py

---
+++ CompressProjects.py
@@ -0,0 +1,8 @@
+'''
+Its hard to manage your storage sometimes when you have too many projects 
+occupying too much space. 
+So here I present to you this amazing script that'll compress your project to 
+50% of its original size.*
+
+*By deleting half the files ;p
+'''
